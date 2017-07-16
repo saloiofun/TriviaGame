5f04db7da6addb1a3993de9d456fb26079b74804
Sandro Wong <saloiofun@gmail.com>
2017-07-16 15:54:26 -0700

    added bg and questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,80 @@
+var trivia =
+{
+    videoGame : [
+    {
+        question: "Which of these is NOT a Super Mario Bros. character?",
+        options: ["Mario","Luigi","Pikachu","Yoshi"],
+        answer: "c"
+    },
+    {
+        question: "The name of Link's legendary sword in the Legend of Zelda series is called what?",
+        options: ["Master Sword","Buster Sword","Ragnarok","Energy Sword"],
+        answer: "a"
+    },
+    {
+        question: "In the game series 'Donkey Kong', what is the name of DK's grandpa?",
+        options: ["Cool Kong","Cranky Kong","Diddy Kong","Grandpa Kong"],
+        answer: "b"
+    },
+    {
+        question: "Which of these is not a pokemon?",
+        options: ["Pikachu","Jigglypuff","Frodo","Squirtle"],
+        answer: "c"
+    },
+    {
+        question: "In the game 'Portal', Chell is offered grief counseling and ________ for completing the tests?",
+        options: ["cake","$5,000","freedom","a vacation"],
+        answer: "a"
+    },
+    {
+        question: "Which of these is NOT a Left 4 Dead character?",
+        options: ["Zoey","Francis","David","Louis"],
+        answer: "c"
+    },
+    {
+        question: "In 'Resident Evil 4', Leon has to save and escort who across a European village?",
+        options: ["The president","The president's daughter","His wife","A scientist"],
+        answer: "b"
+    },
+    {
+        question: "Which of these video game consoles came out first?",
+        options: ["Playstation","X Box","Sega Saturn","Nintendo 64"],
+        answer: "a"
+    },
+    {
+        question: "In 'Super Smash Bros Melee', who is the last possible character that can be unlocked?",
+        options: ["Mr. Game & Watch","Mewtwo","Ganondorf","Roy"],
+        answer: "a"
+    },
+    {
+        question: "Which of the following video game series is the oldest?",
+        options: ["Resident Evil","Metal Gear","Sonic the Hedgehog","Final Fantasy"],
+        answer: "b"
+    },
+    {
+        question: "When did the best selling sports franchise FIFA first get made?",
+        options: ["1991","1992","1993","1994 "],
+        answer: "c"
+    },
+    {
+        question: "Which of the following types of controllers does NOT exist?",
+        options: ["Sega Fishing Rod","Nintendo Power Glove","Sony Toolbelt","Resident Evil Chainsaw"],
+        answer: "c"
+    },
+    {
+        question: "What is the name of the founder of Rapture in the game 'Bioshock'",
+        options: ["Cave Johnson","Andrew Ryan","Rory Williams","Albert Wesker"],
+        answer: "b"
+    },
+    {
+        question: "Which of the Following is NOT one of the original boss battles from Megaman?",
+        options: ["Cut Man","Guts Man","Bomb Man","Heat Man"],
+        answer: "d"
+    },
+    {
+        question: "What is the best selling game console of all-time?",
+        options: ["NES","Playstation 2","SNES","Nintedo Wii"],
+        answer: "d"
+    }
+    ]
+}
